6e2fb1030b1769e1e1210b383563ddfb4301d67d
ScalaAlien <ScalaAlien@gmail.com>
2020-07-23 10:37:14 +0000

    rm cmake folder

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /workspaces/c
-# Build directory: /workspaces/c/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
